92c09bf7a1f1f1a739ba00486c0500938c5a37ca
Dave Liddament <daveliddament@gmail.com>
2017-02-21 21:32:28 +0000

    ADD Generate website command

---
+++ app/src/WebsiteGenerator/GenerateWebsiteCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Phpsw\Website\WebsiteGenerator;
+
+use Phpsw\Website\Importer\Importer;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GenerateWebsiteCommand extends Command
+{
+    /**
+     * @var WebsiteGenerator
+     */
+    private $websiteGenerator;
+
+    /**
+     * @var Importer
+     */
+    private $importer;
+
+    /**
+     * ImportCommand constructor.
+     *
+     * @param WebsiteGenerator $websiteGenerator
+     * @param Importer $importer
+     */
+    public function __construct(WebsiteGenerator $websiteGenerator, Importer $importer)
+    {
+        parent::__construct();
+        $this->websiteGenerator = $websiteGenerator;
+        $this->importer = $importer;
+    }
+
+    protected function configure()
+    {
+        $this->setName('website:generate');
+        $this->setDescription('Imports data and generates website.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        try {
+            $output->writeln('Importing data in directory...');
+            $this->importer->import();
+            $output->writeln('Generating website....');
+            $this->websiteGenerator->generateWebsite();
+            $output->writeln('Done');
+        } catch (\Exception $e) {
+            $output->writeln('Failed to generate website:');
+            $output->writeln($e->getMessage());
+        }
+    }
+}
