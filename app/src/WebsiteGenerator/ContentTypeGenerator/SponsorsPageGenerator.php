0b4fc7f553a6d702d7120a9067b812767501193a
Dave Liddament <daveliddament@gmail.com>
2017-11-13 11:48:38 +0000

    ADD sponsor content type page generator

---
+++ app/src/WebsiteGenerator/ContentTypeGenerator/SponsorsPageGenerator.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Phpsw\Website\WebsiteGenerator\ContentTypeGenerator;
+
+use Phpsw\Website\Entity\Person;
+use Phpsw\Website\Repository\PersonRepositoryInterface;
+use Phpsw\Website\Repository\WebsiteInfoRepositoryInterface;
+use Phpsw\Website\WebsiteGenerator\TemplateRenderer\TemplateRenderer;
+
+
+/**
+ * Generates:
+ *
+ * - sponsors page (this shows only the current sponsors)
+ */
+class SponsorsPageGenerator implements ContentTypeGeneratorsInterface
+{
+    /**
+     * @var WebsiteInfoRepositoryInterface
+     */
+    private $websiteInfoRepository;
+
+    /**
+     * SponsorsPageGenerator constructor.
+     * @param WebsiteInfoRepositoryInterface $websiteInfoRepository
+     */
+    public function __construct(WebsiteInfoRepositoryInterface $websiteInfoRepository)
+    {
+        $this->websiteInfoRepository = $websiteInfoRepository;
+    }
+
+
+    /**
+     * Generate pages for content type.
+     *
+     * This includes:
+     * - a page listing current sponsors
+     *
+     * {@inheritdoc}
+     */
+    public function generatePages(TemplateRenderer $templateRenderer)
+    {
+        $websiteInfo = $this->websiteInfoRepository->getWebsiteInfo();
+
+        $templateRenderer->render('sponsors.html', 'sponsors', [
+            'sponsors' => $websiteInfo->getSponsors(),
+        ]);
+    }
+
+}
