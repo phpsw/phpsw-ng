de72026c0ddb03db508471a25995c4412d7d5243
Kat <kat.zien@brightpearl.com>
2018-01-09 03:21:35 +0100

    Person JSON file generator command implementation

--- app/src/DataFileGenerator/GeneratePersonFileCommand.php
+++ app/src/DataFileGenerator/GeneratePersonFileCommand.php
@@ -2,55 +2,153 @@
 
 namespace Phpsw\Website\DataFileGenerator;
 
+use Phpsw\Website\Entity\Person;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 
 class GeneratePersonFileCommand extends Command
 {
-    private $meetupAPIClient;
-
     /** @var PersonFileGenerator */
     private $personFileGenerator;
 
     /**
      * GeneratePersonFileCommand constructor.
-     *
-     * @param $meetupAPIClient
-     * @param PersonFileGenerator $speakerFileGenerator
+     * @param PersonFileGenerator $personFileGenerator
      */
-    public function __construct(PersonFileGenerator $speakerFileGenerator)
+    public function __construct(PersonFileGenerator $personFileGenerator)
     {
         parent::__construct();
-//        $this->meetupAPIClient = $meetupAPIClient;
-        $this->personFileGenerator = $speakerFileGenerator;
+        $this->personFileGenerator = $personFileGenerator;
     }
 
     protected function configure()
     {
-        $this->setName('phpsw:generate-file-speaker');
-        $this->setDescription('Generates a speaker JSON file with as much data from Meetup as possible.');
+        $this->setName('phpsw:generate-file-person');
+        $this->setDescription('Generates a person JSON file, optionally pulling in as much data from Meetup as possible.');
         $this->addArgument('meetupId', InputArgument::OPTIONAL, 'Does this person have a Meetup ID?');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         try {
-            $meetupID = $input->getArgument('meetupId');
+            $meetupId = $input->getArgument('meetupId');
 
-            if ($meetupID) {
-                $output->writeln("Pulling data from Meetup for user {$meetupID}...");
+            if ($meetupId) {
+                $output->writeln("Pulling data from Meetup for user {$meetupId}...");
+                try {
+                    $person = $this->personFileGenerator->getPersonFromMeetup($meetupId);
+                } catch (\Exception $e) {
+                    $output->writeln('Failed to retrieve the member from Meetup:');
+                    $output->writeln($e->getMessage());
+                    return;
+                }
+            } else {
+                $person = new Person();
             }
 
-            // query meetup and fill in the data
+            $this->checkMissingData($person, $input, $output);
 
-//            $this->personFileGenerator->generatePersonFile();
+            $filePath = $this->personFileGenerator->generateFile($person);
 
-            $output->writeln('Done');
+            $output->writeln("<info>File saved in {$filePath}</info>");
+
         } catch (\Exception $e) {
-            $output->writeln('Failed to generate a speaker file:');
+            $output->writeln('<error>Failed to generate a person file:</error>');
             $output->writeln($e->getMessage());
         }
+    }
+
+    /**
+     * Checks if any of the data is missing and prompts the user to enter at least the required info.
+     * @param Person $person
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @throws \Exception
+     */
+    private function checkMissingData(Person $person, InputInterface $input, OutputInterface $output)
+    {
+        $missing = false;
+
+        if (empty($person->getName())) {
+            $missing = true;
+            $output->writeln("<error>Missing name (required)</error>");
+
+            $name = $this->askUserQuestion('Enter the full name of this person: ', $input, $output);
+            if (empty($name)) {
+                throw new \Exception('missing required full name');
+            }
+            $person->setName($name);
+        }
+
+        if (empty($person->getDescription())) {
+            $missing = true;
+            $output->writeln("<error>Missing description (required)</error>");
+
+            $description = $this->askUserQuestion('Enter this person\'s description: ', $input, $output);
+            if (empty($description)) {
+                throw new \Exception('missing required description');
+            }
+            $person->setDescription($description);
+        }
+
+        if (empty($person->getPhotoUrl())) {
+            $missing = true;
+            $output->writeln("<comment>Missing photo URL</comment>");
+            $photoUrl = $this->askUserQuestion('Enter photo URL or leave blank to skip: ', $input, $output);
+            if (!empty($photoUrl)) {
+                $person->setPhotoUrl($photoUrl);
+            }
+        }
+
+        if (empty($person->getWebsiteUrl())) {
+            $missing = true;
+            $output->writeln("<comment>Missing website URL</comment>");
+            $websiteUrl = $this->askUserQuestion('Enter website URL or leave blank to skip: ', $input, $output);
+            if (!empty($websiteUrl)) {
+                $person->setWebsiteUrl($websiteUrl);
+            }
+        }
+
+        if (empty($person->getTwitterHandle())) {
+            $missing = true;
+            $output->writeln("<comment>Missing twitter handle</comment>");
+            $twitter = $this->askUserQuestion('Enter twitter handle or leave blank to skip: ', $input, $output);
+            if (!empty($twitter)) {
+                $person->setTwitterHandle($twitter);
+            }
+        }
+
+        if (empty($person->getGithubHandle())) {
+            $missing = true;
+            $output->writeln("<comment>Missing github handle</comment>");
+            $github = $this->askUserQuestion('Enter github handle or leave blank to skip: ', $input, $output);
+            if (!empty($github)) {
+                $person->setGithubHandle($github);
+            }
+        }
+
+        if (empty($person->getMeetupId())) {
+            $missing = true;
+            $output->writeln("<comment>Missing meetup ID</comment>");
+            $meetupId = $this->askUserQuestion('Enter meetup ID or leave blank to skip: ', $input, $output);
+            if (!empty($meetupId)) {
+                $person->setMeetupId($meetupId);
+            }
+        }
+
+        if (!$missing) {
+            $output->writeln("<info>All data available</info>");
+        }
+    }
+
+    private function askUserQuestion($question, InputInterface $input, OutputInterface $output)
+    {
+        $helper = $this->getHelper('question');
+        $q = new Question($question, null);
+
+        return $helper->ask($input, $output, $q);
     }
 }
--- app/src/DataFileGenerator/PersonFileGenerator.php
+++ app/src/DataFileGenerator/PersonFileGenerator.php
@@ -2,7 +2,129 @@
 
 namespace Phpsw\Website\DataFileGenerator;
 
+use Phpsw\Website\Entity\Person;
+
 class PersonFileGenerator
 {
+    const GITHUB_BASE_URL = 'https://github.com/';
 
+    /** @var MeetupApiClient */
+    private $meetupAPIClient;
+
+    /**
+     * PersonFileGenerator constructor.
+     * @param MeetupApiClient $meetupAPIClient
+     */
+    public function __construct(MeetupApiClient $meetupAPIClient)
+    {
+        $this->meetupAPIClient = $meetupAPIClient;
+    }
+
+    /**
+     * @param int $meetupId
+     * @return Person
+     * @throws \Exception
+     */
+    public function getPersonFromMeetup(int $meetupId)
+    {
+        $result = $this->meetupAPIClient->getProfiles(['member_id' => $meetupId, 'group_urlname' => 'php-sw']);
+        if (count($result->results) !== 1) {
+            throw new \Exception("more than one member profile returned for id {$meetupId}");
+        }
+
+        $member = $result->results[0];
+
+        $person = new Person();
+        $person->setMeetupId($meetupId);
+
+        if (!empty($member->name)) {
+            $person->setName($member->name);
+        }
+
+        if (!empty($member->photo->highres_link)) {
+            $person->setPhotoUrl($member->photo->highres_link);
+        }
+
+        if (!empty($member->bio)) {
+            $person->setDescription($member->bio);
+        }
+
+        if (!empty($member->other_services->twitter->identifier)) {
+            $person->setTwitterHandle(str_replace('@', '', $member->other_services->twitter->identifier));
+        } else {
+            // /profiles seems to not be returning the social networks info for anyone, so try the /members endpoint
+            $r = $this->meetupAPIClient->getMembers(['member_id' => $meetupId]);
+            if (count($r->results) !== 1) {
+                throw new \Exception("more than one member returned for id {$meetupId}");
+            }
+
+            $m = $r->results[0];
+
+            if (!empty($m->other_services->twitter->identifier)) {
+                $person->setTwitterHandle(str_replace('@', '', $m->other_services->twitter->identifier));
+            }
+        }
+
+        if (!empty($member->site_url)) {
+            $person->setWebsiteUrl($member->site_url);
+        }
+
+        $person->setGithubHandle($this->guessGithub($person));
+
+        return $person;
+    }
+
+    /**
+     * Tries the twitter handle and the slug as the github ID,
+     * if either works the full URL is returned. Otherwise returns null.
+     * @param Person $person
+     * @return string|null
+     */
+    private function guessGithub(Person $person)
+    {
+        if (!empty($person->getTwitterHandle())) {
+            if ($fp = curl_init(self::GITHUB_BASE_URL . $person->getTwitterHandle())) {
+                return self::GITHUB_BASE_URL . $person->getTwitterHandle();
+            }
+        }
+
+        if (!empty($person->getSlug())) {
+            if ($fp = curl_init(self::GITHUB_BASE_URL . $person->getSlug())) {
+                return self::GITHUB_BASE_URL . $person->getSlug();
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @param Person $person
+     * @return string
+     * @throws \Exception
+     */
+    public function generateFile(Person $person)
+    {
+        $fileName = strtolower(str_replace(' ', '-', $person->getName())) . '.json';
+
+        if ($fileName === '.json') {
+            throw new \Exception("no filename available for this file - does this person have a name?");
+        }
+        $filePath = './data/people/' . $fileName;
+
+        $data = [
+            "name"           => $person->getName(),
+            "photo-url"      => $person->getPhotoUrl(),
+            "description"    => $person->getDescription(),
+            "twitter-handle" => $person->getTwitterHandle(),
+            "github-handle"  => $person->getGithubHandle(),
+            "website-url"    => $person->getWebsiteUrl(),
+            "meetup-id"      => $person->getMeetupId(),
+        ];
+
+        $handle = fopen($filePath, 'w') or die('Cannot open file:  ' . $filePath);
+        fwrite($handle, json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+        fclose($handle);
+
+        return $filePath;
+    }
 }
