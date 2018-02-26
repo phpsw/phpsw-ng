09754df2b855d65c849fe0b999c4fea935f69778
Kat <kat.zien@brightpearl.com>
2018-02-26 21:24:12 +0000

    Remove unused bits of the event generator

--- app/src/DataFileGenerator/GenerateEventFileCommand.php
+++ app/src/DataFileGenerator/GenerateEventFileCommand.php
@@ -7,7 +7,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\Question;
 
 class GenerateEventFileCommand extends Command
 {
@@ -51,8 +50,6 @@ class GenerateEventFileCommand extends Command
                 $event = new Event();
             }
 
-//            $this->addMissingData($event, $input, $output);
-
             $filePath = $this->eventFileGenerator->generateFile($event);
 
             $output->writeln("<info>File saved in {$filePath}</info>");
@@ -60,98 +57,5 @@ class GenerateEventFileCommand extends Command
             $output->writeln('<error>Failed to generate an event file:</error>');
             $output->writeln($e->getMessage());
         }
-    }
-
-    /**
-     * Checks if any of the data is missing and prompts the user to enter at least the required info.
-     *
-     * @param Event $event
-     * @param InputInterface $input
-     * @param OutputInterface $output
-     *
-     * @throws \Exception
-     */
-    private function addMissingData(Event $event, InputInterface $input, OutputInterface $output)
-    {
-//        $missing = false;
-//
-//        if (empty($event->getName())) {
-//            $missing = true;
-//            $output->writeln('<error>Missing name (required)</error>');
-//
-//            $name = $this->askUserQuestion('Enter the full name of this person: ', $input, $output);
-//            if (empty($name)) {
-//                throw new \Exception('missing required full name');
-//            }
-//            $event->setName($name);
-//        }
-//
-//        if (empty($event->getDescription())) {
-//            $missing = true;
-//            $output->writeln('<error>Missing description (required)</error>');
-//
-//            $description = $this->askUserQuestion('Enter this person\'s description: ', $input, $output);
-//            if (empty($description)) {
-//                throw new \Exception('missing required description');
-//            }
-//            $event->setDescription($description);
-//        }
-//
-//        if (empty($event->getPhotoUrl())) {
-//            $missing = true;
-//            $output->writeln('<comment>Missing photo URL</comment>');
-//            $photoUrl = $this->askUserQuestion('Enter photo URL or leave blank to skip: ', $input, $output);
-//            if (!empty($photoUrl)) {
-//                $event->setPhotoUrl($photoUrl);
-//            }
-//        }
-//
-//        if (empty($event->getWebsiteUrl())) {
-//            $missing = true;
-//            $output->writeln('<comment>Missing website URL</comment>');
-//            $websiteUrl = $this->askUserQuestion('Enter website URL or leave blank to skip: ', $input, $output);
-//            if (!empty($websiteUrl)) {
-//                $event->setWebsiteUrl($websiteUrl);
-//            }
-//        }
-//
-//        if (empty($event->getTwitterHandle())) {
-//            $missing = true;
-//            $output->writeln('<comment>Missing twitter handle</comment>');
-//            $twitter = $this->askUserQuestion('Enter twitter handle or leave blank to skip: ', $input, $output);
-//            if (!empty($twitter)) {
-//                $event->setTwitterHandle($twitter);
-//            }
-//        }
-//
-//        if (empty($event->getGithubHandle())) {
-//            $missing = true;
-//            $output->writeln('<comment>Missing github handle</comment>');
-//            $github = $this->askUserQuestion('Enter github handle or leave blank to skip: ', $input, $output);
-//            if (!empty($github)) {
-//                $event->setGithubHandle($github);
-//            }
-//        }
-//
-//        if (empty($event->getMeetupId())) {
-//            $missing = true;
-//            $output->writeln('<comment>Missing meetup ID</comment>');
-//            $meetupId = $this->askUserQuestion('Enter meetup ID or leave blank to skip: ', $input, $output);
-//            if (!empty($meetupId)) {
-//                $event->setMeetupId($meetupId);
-//            }
-//        }
-//
-//        if (!$missing) {
-//            $output->writeln('<info>All data available</info>');
-//        }
-    }
-
-    private function askUserQuestion($question, InputInterface $input, OutputInterface $output)
-    {
-        $helper = $this->getHelper('question');
-        $q = new Question($question, null);
-
-        return $helper->ask($input, $output, $q);
     }
 }
