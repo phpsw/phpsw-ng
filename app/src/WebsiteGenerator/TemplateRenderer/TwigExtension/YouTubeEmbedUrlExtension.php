8e30051cb4ccf3fd8c4e4cd9a5e635eb01a4f3a7
Lee Stone <lee@leestone.co.uk>
2018-03-29 18:35:00 +0100

    Get YouTube videos showing up on talk pages

---
+++ app/src/WebsiteGenerator/TemplateRenderer/TwigExtension/YouTubeEmbedUrlExtension.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lee.stone
+ * Date: 29/03/2018
+ * Time: 17:51
+ */
+
+namespace Phpsw\Website\WebsiteGenerator\TemplateRenderer\TwigExtension;
+
+
+use Phpsw\Website\Entity\Talk;
+use Twig_Extension;
+use Twig_SimpleFunction;
+
+class YouTubeEmbedUrlExtension extends Twig_Extension
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return [
+            new Twig_SimpleFunction('youTubeEmbedUrl', [$this, 'getYouTubeEmbedUrl']),
+        ];
+    }
+
+    /**
+     * Returns the URL which can be used for a YouTube iFrame embed
+     *
+     * @param Talk $talk
+     *
+     * @return string
+     */
+    public function getYouTubeEmbedUrl(Talk $talk): string
+    {
+        preg_match("/^(?:http(?:s)?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>]+)/", $talk->getVideoUrl(), $matches);
+
+        return $matches[1];
+    }
+
+}
